3c604e423558be2b052737acab5764e319be42f6
Kyle Schulz <ryoken@ryoken.net>
2011-02-19 22:39:31 -0700

    Fixed the sally verification

--- project05/app/models/article.rb
+++ project05/app/models/article.rb
@@ -2,5 +2,15 @@ class Article < ActiveRecord::Base
 	validates_presence_of :title, :author, :body, :message => '- may not be blank.'
 	validates_uniqueness_of :title, :message => '- there is already an article with this title.'
 	validates_uniqueness_of :body, :message => '- this article has already been posted under another title.'
-	validates_exclusion_of :author, :in => 'Sally', :message => '- No Sally\'s allowed!'
+	validate :no_sally
+
+private
+	
+	def add_count
+	end
+	
+	def no_sally
+		errors.add_to_base("Sally may not be an author.") if author.downcase.include? "sally"
+	end
+
 end
