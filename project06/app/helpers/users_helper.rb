bab8f95efc8fa4eb19ad8bdd09072d74fa7d9c08
Kyle Schulz <ryoken@ryoken.net>
2011-02-27 21:41:37 -0700

    Fixed reCaptcha again...

--- project06/app/helpers/users_helper.rb
+++ project06/app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+
+	def validation_code(user)
+		out=""
+		if !current_user
+			out  << recaptcha_tags
+		end
+		out.html_safe
+	end
+
 end
--- project06/app/models/user.rb
+++ project06/app/models/user.rb
@@ -1,9 +1,14 @@
 class User < ActiveRecord::Base
 
+	belongs_to :role
 	attr_accessible :username, :password, :password_confirmation, :photo
 	acts_as_authentic
 	cattr_reader :per_page
 	@@per_page = 10
-	has_attached_file :photo, :styles => { :small => "150x150>" }, :url => "/assets/:class/:attachment/:id/:style/:filename"
+	has_attached_file :photo, :styles => { :small => "300x300>", :icon => "50x50>" }, :url => "/assets/:class/:attachment/:id/:style/:filename"
+
+	def role_symbols
+		return [role.name.to_sym]
+	end
 
 end
